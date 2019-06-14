33a60f192000c4cd35b57bf98b4b19a87bceba7c
micsson <michaelperssonmail@gmail.com>
2019-06-14 19:18:51 +0200

    begin fixing unittest

---
+++ src/js/createOption.spec.js
@@ -0,0 +1,8 @@
+describe('unit test createOption', () => {
+    test('should create an option element', () => {
+      const createOption = require('./animalApp.js').createOption;
+      const testOption = createOption('test', 'Test');
+      const option = '<option value="test">Test</option>';
+      expect(testOption.outerHTML).toEqual(option);
+    });
+  });
